8190d88b57ef0c646fcf3c3050a1f1d5d88f7b8d
RosVor <ros.vorokh@gmail.com>
2024-01-17 21:20:59 +0200

    Useless router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { NavLink } from 'react-router-dom';
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import Movies from './Movies';
 import MovieDetails from './MovieDetails';
@@ -28,17 +28,15 @@ const App = () => {
         </nav>
       </header>
       <main>
-        <Routes>
+      <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/movies" element={<Movies />} />
           <Route path="/movies/:movieId" element={<MovieDetails />} />
           <Route path="/movies/:movieId/cast" element={<Cast />} />
           <Route path="/movies/:movieId/reviews" element={<Reviews />} />
-
         </Routes>
       </main>
       <footer>
-
       </footer>
     </div>
   );
