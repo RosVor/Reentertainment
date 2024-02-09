52114a66f5553be4dcd39271e0ca86a1ba0c33b5
RosVor <ros.vorokh@gmail.com>
2024-02-09 02:08:59 +0200

    MD: return,img.err

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { useParams, Link, Outlet, useNavigate } from 'react-router-dom';
+import { useParams, Link, Outlet, useNavigate, useLocation } from 'react-router-dom';
 import { getMovieDetails } from '../components/api'; 
 
 const MovieDetails = () => {
   const { movieId } = useParams();
   const [movieDetails, setMovieDetails] = useState(null);
   const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
     const fetchMovieDetails = async () => {
@@ -24,11 +25,17 @@ const MovieDetails = () => {
     return <div>Loading...</div>;
   }
 
+  const backLinkHref = location.state?.from ?? "/";
+
   return (
     <div>
-      <button onClick={() => navigate(-1)}>Return</button>
+      <button onClick={() => navigate(backLinkHref)}>Return</button>
       <div className='movies-details'>
-        <img src={`https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`} alt={movieDetails.title} />
+      <img 
+          src={`https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`} 
+          alt={movieDetails.title} 
+          onError={(e) => {e.target.onerror = null; e.target.src="https://source.unsplash.com/random/500x750"}}
+        />
         <div className='movies-details-info'>
           <h2>{movieDetails.title}</h2>
           <p>User Score: {movieDetails.vote_average}</p>
